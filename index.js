25e2ae585d325e3ca3a611d9a5b94061e06c995f
lovleenkaur <lovleenk99@gmail.com>
2023-06-16 10:40:18 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(target="roller-skate"){ 
+    return(`This Saturday, I want to ${target}!`)
+}
+
+const mondayWork= function mondayWork(target="go to the office") {
+    return(`This Monday, I will ${target}.`)
+}
+
+let wrapAdjective = function wrapAdjective (target = "*") { 
+    return function (special= "a hard worker") { 
+        return(`You are ${target}${special}${target}!`)
+    }
+}
+
